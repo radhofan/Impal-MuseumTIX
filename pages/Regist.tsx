9c68dace87818554bd87bd54ddb83e5d09ce8ebb
radyarad <muh.syahradya@gmail.com>
2024-12-11 15:34:43 +0700

    RadoJawa

---
+++ pages/Regist.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import '@/app/globals.css'; // Your global styles
+import Regist from '@/components/LoginRegist/Regist';
+
+const loginRegistWrapper = () => {
+  return <Regist />;
+};
+
+export default loginRegistWrapper;
