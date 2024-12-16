4e2f998eda55a38ff7d45b91deec0743a83f6f38
Labib Hakam Fauzi <labibhakam.fauzi@gmail.com>
2024-12-16 11:10:40 +0700

    Update Detail Museum dan detail tiket museum (Hakam)

---
+++ pages/DetailTiketMuseum.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DetailMuseum from '@/components/DetailMuseum/DetailTiketMuseum';
+
+const DetailMuseumWrapper = () => {
+  return <DetailMuseum/>;
+};
+
+export default DetailMuseumWrapper;
