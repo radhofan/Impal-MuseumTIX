b7bb0a8f3f4b6c578306a61ada0545ae13319bcc
Radho <radhofanazizi@gmail.com>
2025-01-01 15:32:19 +0700

    ada

--- pages/OrderPage/[id].tsx
+++ pages/OrderPage/[id].tsx
@@ -1,3 +1,29 @@
+// import React, { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+// import '@/app/globals.css'; 
+// import OrderPage from '@/components/OrderPage/OrderPage';
+
+// const OrderPageWrapper = () => {
+//   const router = useRouter();
+//   const { id } = router.query;
+  
+//   const [museumId, setMuseumId] = useState(null);
+
+//   useEffect(() => {
+//     if (id) {
+//       setMuseumId(id); // Update state when 'id' is available
+//     }
+//   }, [id]); // Only update when 'id' changes
+
+//   if (!museumId) {
+//     return <div>Loading...</div>; // Show loading until the id is available
+//   }
+
+//   return <OrderPage museum_id={museumId} />;
+// };
+
+// export default OrderPageWrapper;
+
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import '@/app/globals.css'; 
@@ -7,16 +33,19 @@ const OrderPageWrapper = () => {
   const router = useRouter();
   const { id } = router.query;
   
-  const [museumId, setMuseumId] = useState(null);
+  const [museumId, setMuseumId] = useState<number | null>(null);
 
   useEffect(() => {
     if (id) {
-      setMuseumId(id); // Update state when 'id' is available
+      const numId = parseInt(Array.isArray(id) ? id[0] : id, 10);
+      if (!isNaN(numId)) {
+        setMuseumId(numId);
+      }
     }
-  }, [id]); // Only update when 'id' changes
+  }, [id]);
 
   if (!museumId) {
-    return <div>Loading...</div>; // Show loading until the id is available
+    return <div>Loading...</div>;
   }
 
   return <OrderPage museum_id={museumId} />;
