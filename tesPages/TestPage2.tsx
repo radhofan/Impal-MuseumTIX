9dd0307e64a840fbee98adf53e108f2ec930f880
Radho <radhofanazizi@gmail.com>
2025-01-01 15:34:22 +0700

    ada

--- tesPages/TestPage2.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TestPage2 = () => {
-  return (
-    <div>TestPage2</div>
-  )
-}
-
-export default TestPage2
--- tesPages/TestPage3.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TestPage3 = () => {
-  return (
-    <div>TestPage3</div>
-  )
-}
-
-export default TestPage3
