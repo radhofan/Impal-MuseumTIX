31cd5e3ce427104230f258730e9fb4f814aa9bae
Radho <radhofanazizi@gmail.com>
2025-01-01 12:51:33 +0700

    ada

--- config.js
+++ config.js
@@ -3,10 +3,12 @@ const config = {
       apiUrl: "http://localhost:9090", // Local API URL
     },
     production: {
-      apiUrl: "https://your-deployed-api-url.com", // Production API URL
+      apiUrl: "http://mtixbackend-production.up.railway.app", // Production API URL
     },
   };
   
-  const environment = process.env.NODE_ENV || 'development'; // Get the environment (default to 'development')
+  //const environment = process.env.NODE_ENV || 'development'; 
+  //const environment = 'development'; 
+  const environment = 'production'; 
   
   export const configUrl = config[environment].apiUrl;
